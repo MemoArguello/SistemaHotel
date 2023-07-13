8ad3985ac7dcf9a398f371d50d2de5d7c8da25b7
Guillermo <elmemozac6@gmail.com>
2023-07-13 13:23:25 -0400

    cambios

---
+++ Frontend/categoria/RegistrarCategoria.php
@@ -0,0 +1,77 @@
+<?php
+session_start();
+include '../../Backend/config/baseDeDatos.php';
+
+$usuario = $_SESSION['usuario'];
+if (!isset($usuario)) {
+    header("location:../../index.php");
+}
+$conexiondb = conectardb();
+    $sql = "SELECT id_cargo FROM `usuarios` WHERE usuario = '$usuario';";
+    $result = mysqli_query($conexiondb, $sql);
+    while ($usuario= mysqli_fetch_assoc($result)) {
+        if ($usuario['id_cargo'] != 1) {
+            header("location:../../index.php");
+        }
+}
+$usuario = $_SESSION['usuario'];
+$conexiondb = conectardb();
+$query = "SELECT * FROM categorias";
+$resultado = mysqli_query($conexiondb, $query);
+mysqli_close($conexiondb);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Categoria</title>
+    <!----======== CSS ======== -->
+    <link rel="stylesheet" href="../CSS/style.css">
+    <link rel="stylesheet" href="../CSS/registrar.css">
+    <link rel="stylesheet" href="../CSS/listado.css">
+
+    <!----===== Iconscout CSS ===== -->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    <link href="./IMG/logo.svg" rel="icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+<?php
+    include($_SERVER['DOCUMENT_ROOT'] . '/SistemaHotel/Frontend/dashboard/menu.php');
+?>
+    <section class="dashboard">
+        <div class="top">
+        <div class="topnav" id="myTopnav">
+                <a href="../habitacion/listado_habitacion.php">Habitaciones Existentes</a>
+                <a href="../habitacion/formulario.php">Registrar Habitacion</a>
+                <a href="./listadoCategoria.php">Categorias</a>
+                <a href="./RegistrarCategoria.php">Registrar Categorias</a>
+            </div>
+        </div>
+
+        <div class="dash-content">
+            <div class="signupFrm">
+                <form action="./guardar_categoria.php" method="POST" class="form_categoria">
+                    <h1 class="title">Registrar Categoria</h1>
+                    <div class="inputContainer">
+                        <input type="text" class="input" placeholder="a" name="nombre">
+                        <label for="" class="label">Nombre Categoria</label>
+                    </div>
+                    <div class="inputContainer">
+                        <input type="text" class="input" placeholder="a" name="piso">
+                        <label for="" class="label">Ingrese Piso</label>
+                    </div>
+                    <input type="submit" class="submitBtn" value="GUARDAR">
+                </form>
+            </div>
+    </section>
+
+    <script src="../../JS/script.js"></script>
+
+</body>
+
+</html>
---
+++ Frontend/categoria/listadoCategoria.php
@@ -0,0 +1,94 @@
+<?php
+session_start();
+include '../../Backend/config/baseDeDatos.php';
+
+$usuario = $_SESSION['usuario'];
+if (!isset($usuario)) {
+    header("location:../../index.php");
+}
+$conexiondb = conectardb();
+    $sql = "SELECT id_cargo FROM `usuarios` WHERE usuario = '$usuario';";
+    $result = mysqli_query($conexiondb, $sql);
+    while ($usuario= mysqli_fetch_assoc($result)) {
+        if ($usuario['id_cargo'] != 1) {
+            header("location:../../index.php");
+        }
+}
+$usuario = $_SESSION['usuario'];
+$conexiondb = conectardb();
+$query = "SELECT * FROM categorias";
+$resultado = mysqli_query($conexiondb, $query);
+mysqli_close($conexiondb);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Categoria</title>
+    <!----======== CSS ======== -->
+    <link rel="stylesheet" href="../CSS/style.css">
+    <link rel="stylesheet" href="../CSS/registrar.css">
+    <link rel="stylesheet" href="../CSS/listado.css">
+
+    <!----===== Iconscout CSS ===== -->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    <link href="./IMG/logo.svg" rel="icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+<?php
+    include($_SERVER['DOCUMENT_ROOT'] . '/SistemaHotel/Frontend/dashboard/menu.php');
+?>
+    <section class="dashboard">
+        <div class="top">
+        <div class="topnav" id="myTopnav">
+                <a href="../habitacion/listado_habitacion.php">Habitaciones Existentes</a>
+                <a href="../habitacion/formulario.php">Registrar Habitacion</a>
+                <a href="./listadoCategoria.php">Categorias</a>
+                <a href="./RegistrarCategoria.php">Registrar Categorias</a>
+            </div>
+        </div>
+
+        <div class="dash-content">
+        <br>
+            <h1 align="center">CATEGORIAS</h1>
+            <table id="example" class="table" style="width:100%">
+                <thead>
+                    <tr>
+                        <th>Nº</th>
+                        <th align="center">Categoria</th>
+                        <th align="center">Piso</th>
+                        <th align="left">Opciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $index = 1;
+                    while ($categoria = mysqli_fetch_assoc($resultado)) {
+                        echo "<tr>";
+                        echo "<tr>";
+                        echo "<tr>";
+                        echo "<tr>";
+                        echo "<th scope ='row'>" . $index++ . "</th>";
+                        echo "<td align= 'center'>" . $categoria['categoria'] . "</td>";
+                        echo "<td align= 'center'>" . $categoria['piso'] . "</td>";
+                        echo "<td>";
+                        echo "<a href='./editar_categoria.php?id_categoria=" . $categoria['id_categoria'] . "' class='submitBoton'> Editar </a>";
+                        echo "<a href='./eliminar_categoria.php?id_categoria=" . $categoria['id_categoria'] . "' class='submitBotonEliminar'> Borrar </a>";
+                        echo "</td>";
+                        echo "</tr>";
+                    }
+                    ?>
+                </tbody>
+            </table>
+    </section>
+    <script src="../../JS/script.js"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.j"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
+</body>
+</html>
